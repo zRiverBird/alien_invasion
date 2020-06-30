21a758dd33c7b056f60e7ca5ebf1ec96e33fc611
zriverbird <774986925@qq.com>
2020-06-30 20:45:54 +0800

    fourth commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,13 +22,14 @@ def run_game():
     #创建一个用于存储外星人的编组
     aliens = Group()
     #创建外星人群
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
+        gf.update_aliens(aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
