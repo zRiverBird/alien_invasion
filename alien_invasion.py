a412961e6b6b72f89683919101e8c318ded69563
zriverbird <774986925@qq.com>
2020-06-30 15:30:27 +0800

    thirth commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,19 +19,17 @@ def run_game():
     ship = Ship(ai_settings, screen)
     #创建一个用于存储子弹的编组
     bullets = Group()
+    #创建一个用于存储外星人的编组
+    aliens = Group()
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-
-        #删除已消失的子弹
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
