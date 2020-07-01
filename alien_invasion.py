5e56e665e982cd7aeb77dd3079931010a3425419
zriverbird <774986925@qq.com>
2020-07-01 15:01:48 +0800

    fifth commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from game_stats import GameStats
+
 from ship import Ship
 import game_functions as gf
 
@@ -15,6 +17,8 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Al ien Invasion")
 
+    #创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
     #创建一艘飞船
     ship = Ship(ai_settings, screen)
     #创建一个用于存储子弹的编组
@@ -28,8 +32,8 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_aliens(aliens)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
--- bullet.py
+++ bullet.py
@@ -1,8 +1,9 @@
-import pygame
-from pygame.sprite import Sprite
 """
     一个对飞船发射的子弹进行管理的类
 """
+
+import pygame
+from pygame.sprite import Sprite
 
 
 class Bullet(Sprite):
